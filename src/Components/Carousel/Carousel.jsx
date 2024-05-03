764789a6f29f897b389d468e7f3296b4085262a5
jaikrishnavr01 <jaikrishnavr@outlook.com>
2024-05-03 22:36:34 +0530

    carousel

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import './carousel.css';
+import img1 from '../../assets/bedroom.jpg';
+import img2 from '../../assets/hotel2.jpg';
+import img3 from '../../assets/hotel3.jpg';
 
 function Carousel() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slides = [
+    img1,
+    img2,
+    img3
+  ]; 
+
+  const goToPreviousSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === 0 ? slides.length - 1 : prevSlide - 1));
+  };
+
+  const goToNextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === slides.length - 1 ? 0 : prevSlide + 1));
+  };
+
+  useEffect(() => {
+    const interval = setInterval(goToNextSlide, 5000); 
+    return () => clearInterval(interval);
+  }, [currentSlide]);
+
   return (
-    <div>Carousel</div>
-  )
+    <div className="carousel">
+      <div className="label">
+        <h3>Welcome to H-Residency luxury hotel</h3>
+        <p>Delight in life's pinnacle moments</p>
+      </div>
+      <button onClick={goToPreviousSlide}>{"<"}</button>
+      <div className="slide">
+        <img src={slides[currentSlide]} alt={`Slide ${currentSlide + 1}`} />
+      </div>
+      <button onClick={goToNextSlide}> {">"} </button>
+    </div>
+  );
 }
 
-export default Carousel
+export default Carousel;
