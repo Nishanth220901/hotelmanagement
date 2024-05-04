d66d0f98c2010f083ff1af2f2c2bcc7e53c324fa
KGM <KGM@CASL6-39-167.kgisledu.com>
2024-05-03 23:01:00 -0700

    contact us

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
-import Navbar from '../Components/Navbar/Navbar'
+import React from 'react';
+import Navbar from '../Components/Navbar/Navbar';
+import Footer from '../Components/Footer/Footer';
+import './Contact.css'
 
 function Contact() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <div className='container'>
+        <h1>Contact-us</h1>
+        <div className='row'>
+          <div className='col'>
+            <h5>Contact</h5>
+            <div>
+              <i className="fa-solid fa-location-dot"></i>
+              <h3>123 Blissful Avenue Saravanampatti, Coimbatore, Tamilnadu-641035, India</h3>
+            </div>
+            <div>
+              <i className="fa-solid fa-phone"></i>
+              <h3>+123 456 7890</h3>
+            </div>
+            <div>
+              <i className="fa-solid fa-envelope"></i>
+              <h3>info@hresidency.com</h3>
+            </div>
+          </div>
+          <div className='col'>
+            <form>
+              <label>Name</label>
+              <input type="text" placeholder='Enter your name' />
+              <br />
+              <label>Email</label>
+              <input type="email" placeholder='Enter your email' /> 
+              <br />
+              <label>Comments</label>
+              <textarea cols="30" rows="10"></textarea>
+              <button type='Submit'>Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
