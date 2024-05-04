acf9650a690503559ab8c75f7bb1992a71d89ca7
KGM <KGM@CASL6-39-167.kgisledu.com>
2024-05-03 23:09:01 -0700

    contact us

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -8,21 +8,21 @@ function Contact() {
     <div>
       <Navbar />
       <div className='container'>
-        <h1>Contact-us</h1>
+        <h1 className='h1'>Contact-us</h1>
         <div className='row'>
           <div className='col'>
-            <h5>Contact</h5>
+            <h5 className='.h5'>Contact</h5>
             <div>
               <i className="fa-solid fa-location-dot"></i>
-              <h3>123 Blissful Avenue Saravanampatti, Coimbatore, Tamilnadu-641035, India</h3>
+              <h3 className='h3'>123 Blissful Avenue Saravanampatti, Coimbatore, Tamilnadu-641035, India</h3>
             </div>
             <div>
               <i className="fa-solid fa-phone"></i>
-              <h3>+123 456 7890</h3>
+              <h3 className='h3'>+123 456 7890</h3>
             </div>
             <div>
               <i className="fa-solid fa-envelope"></i>
-              <h3>info@hresidency.com</h3>
+              <h3 className='h3'>info@hresidency.com</h3>
             </div>
           </div>
           <div className='col'>
@@ -34,8 +34,8 @@ function Contact() {
               <input type="email" placeholder='Enter your email' /> 
               <br />
               <label>Comments</label>
-              <textarea cols="30" rows="10"></textarea>
-              <button type='Submit'>Submit</button>
+              <textarea className='textarea' cols="30" rows="10"></textarea>
+              <button className='button' type='Submit'>Submit</button>
             </form>
           </div>
         </div>
